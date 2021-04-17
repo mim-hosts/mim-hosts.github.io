f50a5886c2c93398d41e9da449c761623aef5613
Kacper Rokicki <kacper.rokicki@cloudtechnologies.pl>
2021-04-17 02:52:18 +0200

    super

---
+++ src/Header/Header.tsx
@@ -0,0 +1,40 @@
+import {AppBar, Link, makeStyles, Toolbar, Typography} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#343a40",
+        height: "56px"
+    },
+    logo: {
+        fontFamily: "Titillium Web, sans-serif",
+        fontWeight: 600,
+        color: "#fff",
+        paddingBottom: ".5125rem",
+        fontSize: "1.25rem",
+    }
+}));
+
+function Header() {
+    const { header, logo } = useStyles();
+
+    const displayDesktop = () => {
+        return <Toolbar>{appLogo}</Toolbar>;
+    };
+
+    const appLogo = (
+        <Link href="https://mim-hosts.github.io" style={{ textDecoration: 'none' }}>
+            <Typography className={logo}>
+                mim-hosts.github.io
+            </Typography>
+        </Link>
+    );
+
+    return (
+        <header>
+            <AppBar className={header}>{displayDesktop()}</AppBar>
+        </header>
+    );
+}
+
+export default Header;
